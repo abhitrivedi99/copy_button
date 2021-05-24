8fea597090e7e2935ddde399005db94e37b47b38
abhiS99 <abhishek@solulab.co>
2021-05-24 13:17:27 +0530

    Final build

--- src/App.js
+++ src/App.js
@@ -1,25 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+const TOKEN =
+	'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2MDA4MDhlNGQwOTU4ODBlODE4ZjRjMjkiLCJlbWFpbCI6InV0cGFsQHNvbHVsYWIuY29tIiwidXNlcm5hbWUiOiJ1dHBhbEBzb2x1bGFiLmNvbSIsImxhc3RuYW1lIjoiQmV0YWkiLCJmaXJzdG5hbWUiOiJVdHBhbCIsImlhdCI6MTYyMTYwNjQ3OCwiZXhwIjoxNjIyODE2MDc4fQ.gvBFoMp_CfpaxeKzJiNREK2voJXrIbkJ-rVIUcHPCWU'
+const ID = '600808e4d095880e818f4c29'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default function App() {
+	return (
+		<div>
+			<div className="ui four column doubling stackable grid container">
+				<div className="column">
+					<button
+						class="ui primary button"
+						onClick={() => navigator.clipboard.writeText(TOKEN)}
+					>
+						Token
+					</button>
+				</div>
+				<div className="column">
+					<button
+						class="ui  button"
+						onClick={() => navigator.clipboard.writeText(ID)}
+					>
+						ID
+					</button>
+				</div>
+			</div>
+		</div>
+	)
 }
-
-export default App;
